bff1f12e8ba349192b74dfd5f1f02b0c3012cf86
Denni <dennis.steinbach@outlook.com>
2026-01-17 19:55:33 +0100

    Version 11 - json_editor.py

--- json_editor.py
+++ json_editor.py
@@ -68,6 +68,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/img/fav.png">
     <title>JSON Editor – Events & Gallery</title>
     <style>
         * {
